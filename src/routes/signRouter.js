9050d4d37e16ebeed771b4c95eef0b33527d91ca
MarmoW <115847012+MarmoW@users.noreply.github.com>
2023-01-29 05:32:16 -0300

    small fix on router

--- src/routes/signRouter.js
+++ src/routes/signRouter.js
@@ -5,8 +5,8 @@ import {signInFunction, signUpFunction} from "../controllers/signController.js";
 
 const signRoute = Router();
 
-signRoute.post("sign-in", validSignIn(schemaSignIn) ,signInFunction);
+signRoute.post("/sign-in", validSignIn(schemaSignIn) ,signInFunction);
 
-signRoute.post("sign-up", validSignUp(schemaSignUp) ,signUpFunction);
+signRoute.post("/sign-up", validSignUp(schemaSignUp) ,signUpFunction);
 
 export default signRoute;
